4616a0109b8bc312f2eba9dbd5034f35c1871941
Antonio <tzamora@gmail.com>
2015-01-24 09:14:20 -0600

    outrigger logic

---
+++ Assets/scripts/CarriageLeverController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarriageLeverController : MonoBehaviour {
+
+	public TriggerController LeftTrigger;
+
+	public TriggerController RightTrigger;
+
+	// Use this for initialization
+	void Start () {
+	
+		this.ttAppendLoop ("PumpLeverRoutine", delegate(ttHandler handler){
+
+
+
+		});
+
+	}
+}
---
+++ Assets/scripts/TriggerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerController : MonoBehaviour {
+
+	public bool onEnter = false;
+
+	public Collider2D other = false;
+
+	void OnTriggerEnter2D(Collider2D theOther) {
+		onEnter = true;
+		other = theOther;
+	}
+
+	void OnTriggerExit2D(Collider2D theOther) {
+		onEnter = theOther;
+	}
+
+}
