b8e9b66045896ac802d44bd0441046be2fd8f43a
Antonio <tzamora@gmail.com>
2015-01-24 01:09:24 -0600

    primeros artes

--- Assets/GameJamPack2015/BackgroundScrollerController.cs
+++ Assets/GameJamPack2015/BackgroundScrollerController.cs
@@ -23,6 +23,7 @@ public class BackgroundScrollerController : MonoBehaviour {
 
 	IEnumerator StartMovingBackground()
 	{
+
 		while(true)
 		{
 			background1.transform.Translate(ScrollingSpeed * Time.deltaTime, 0f, 0f);
@@ -35,9 +36,7 @@ public class BackgroundScrollerController : MonoBehaviour {
 			// check if the background is not inside the camera
 			//
 
-			//if(!currentBackground.transform.GetBounds().IsVisibleFrom(GameContext.Get.GameCamera))
 			if(!currentBackground.transform.GetBounds().IsVisibleFrom(Camera.main))
-			//if(!currentBackground.renderer.IsVisibleFrom(GameContext.Get.GameCamera))
 			{
 				Vector3 backgroundWidth = new Vector3(background2.transform.GetBounds().size.x, 0f, 0f);
 
@@ -56,16 +55,5 @@ public class BackgroundScrollerController : MonoBehaviour {
 				currentBackground = background1;
 			}
 		}
-	}
-
-	void OnDrawGizmos() 
-	{
-		Bounds background1Bounds = background1.transform.GetBounds();
-
-		Bounds background2Bounds = background2.transform.GetBounds();
-
-		Gizmos.DrawWireCube(background1Bounds.center, background1Bounds.size);
-
-		Gizmos.DrawWireCube(background2Bounds.center, background2Bounds.size);
 	}
 }
