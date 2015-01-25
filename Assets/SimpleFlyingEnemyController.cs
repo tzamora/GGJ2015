efa3502c11ae972f93fe86a5a9841df2b690859d
Antonio <tzamora@gmail.com>
2015-01-24 20:53:52 -0600

    added dot tween

---
+++ Assets/SimpleFlyingEnemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleFlyingEnemyController : MonoBehaviour {
+
+	public float speed;
+
+	public int side = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		MoveRoutine ();
+	
+	}
+	
+	// Update is called once per frame
+	void MoveRoutine () {
+	
+		this.ttAppendLoop ("MoveRoutine", delegate(){
+
+			transform.Translate(new Vector3(side, 0f, 0f) * Time.deltaTime * speed);
+
+		});
+
+	}
+}
